9d3ec9b12594347cf44465cf82f466d14a9f4836
mstclair <stclairm@spu.edu>
2017-07-25 14:22:05 -0700

    bar chart is on there and I have names but no click amnts

--- app.js
+++ app.js
@@ -3,87 +3,139 @@
 function ProductImage(number) {
   this.name = number;
   this.source = 'images/' + this.name + '.jpg';
-  this.timesShown = 0;//counts time an image is shown
-  this.timesEachImageClicked = 0;
+  this.amountOfShows = 0;
+  this.timesClicked = 0;
   ProductImage.all.push(this);
 }
 
-//var timesCycled = 0;//counts clicks
+ProductImage.totalClicks = 0;//counts clicks
 
 ProductImage.all = []; //array for my images
 
 ProductImage.allNames = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];//pushed into array
 
-for(var i = 0; i < ProductImage.allNames.length; i++){ //loop through the number of image names and create object of new image with
-  // new image from all names array
+var previouslyShown = [];
+
+for(var i = 0; i < ProductImage.allNames.length; i++){
   new ProductImage(ProductImage.allNames[i]); //creates new object with images from all names array
 }
-// if(timesCycled > 2){
-// alert('you are out of clicks');
-// //display a list of the products..
-// document.getElementById('the_image').removeEventListener('click', randomImage);
-// document.getElementById('the_image1').removeEventListener('click', randomImage1);
-// document.getElementById('the_image2').removeEventListener('click', randomImage2);
-// }
+ProductImage.leftImage = document.getElementById('left');
+ProductImage.centerImage = document.getElementById('center');
+ProductImage.rightImage = document.getElementById('right');
+ProductImage.container = document.getElementById('imageContainer');
 
 
- ProductImage.imgEl = document.getElementById('the_image');//gets element id from image 0 spot
- ProductImage.imgEl1 = document.getElementById('the_image1'); //gets element id from image 1 spot
- ProductImage.imgEl2 = document.getElementById('the_image2');
+function makeRandomNumber(){
+  return Math.floor(Math.random() * ProductImage.all.length)
 
-function randomImage() {
-    var randomIndex = Math.floor(Math.random() * ProductImage.all.length);
-    ProductImage.imgEl.src = ProductImage.all[randomIndex].source;
-    ProductImage.imgEl.alt = ProductImage.all[randomIndex].name; ///changes image0
-    ProductImage.all[randomIndex].timesShown += 1;
-    ProductImage.all[randomIndex].timesEachImageClicked += 1;
-    console.log(ProductImage.all[randomIndex].name + ' has been shown ' + ProductImage.all[randomIndex].timesShown + ' times' + ' and clicked: ' + ProductImage.all[randomIndex].timesEachImageClicked);
-    //event for clicking 0/---------------------------------------
+}
+
+function displayImages() {
+
+console.log(previouslyShown, 'previouslyShown images');
+
+  var numbers = [];
+  numbers[0] = makeRandomNumber();
+  numbers[1] = makeRandomNumber();
+  while(numbers[0] === numbers [1]){
+    numbers[1] = makeRandomNumber();
+    console.log('dup found');
+  }
+  numbers[2] = makeRandomNumber();
+  while(numbers[2] === numbers [1] || numbers[2] === numbers[0]){
+    numbers[2] = makeRandomNumber();
+    console.log('dup found');
+  }
+  ProductImage.leftImage.src = ProductImage.all[numbers[0]].source;
+  ProductImage.centerImage.src = ProductImage.all[numbers[1]].source;
+  ProductImage.rightImage.src = ProductImage.all[numbers[2]].source;
+  ProductImage.leftImage.alt = ProductImage.all[numbers[0]].name;
+  ProductImage.centerImage.alt = ProductImage.all[numbers[1]].name;
+  ProductImage.rightImage.alt = ProductImage.all[numbers[2]].name;
+  ProductImage.all[numbers[0]].amountOfShows += 1;
+  ProductImage.all[numbers[1]].amountOfShows += 1;
+  ProductImage.all[numbers[2]].amountOfShows += 1;
+  previouslyShown = numbers;
+  //myChart.labels.push(ProductImage.name);
+
+}
+
+function showList(){
+  var ulEl = document.getElementById('thelist');
+  for(var i = 0; i < ProductImage.all.length; i++)
+  {
+    var liEl = document.createElement('li');
+    liEl.textContent = ProductImage.all[i].name + ' was shown ' + ProductImage.all[i].amountOfShows + ' times was clicked ' + ProductImage.all[i].timesClicked + ' times';
+    ulEl.appendChild(liEl);
+
   }
 
-  function randomImage1(){
-    var randomIndex1 = Math.floor(Math.random() * ProductImage.all.length);
-    ProductImage.imgEl1.src = ProductImage.all[randomIndex1].source;
-    ProductImage.imgEl1.alt = ProductImage.all[randomIndex1].name;
-    ProductImage.all[randomIndex1].timesShown += 1;
-    ProductImage.all[randomIndex1].timesEachImageClicked += 1;
-    console.log(ProductImage.all[randomIndex1].name + ' has been shown ' + ProductImage.all[randomIndex1].timesShown + ' times' + ' and clicked: ' + ProductImage.all[randomIndex1].timesEachImageClicked);
-    //random image for one---------------------------------------
+
+}
+
+function handleClick(e){
+  ProductImage.totalClicks += 1;
+  console.log(ProductImage.totalClicks);
+  for(var i = 0; i < ProductImage.all.length; i ++)
+  {
+    if(e.target.alt === ProductImage.all[i].name){
+      ProductImage.all[i].timesClicked += 1;
+    }
   }
+  if(ProductImage.totalClicks === 3){
+    ProductImage.container.removeEventListener('click', handleClick);
+    //set source attributes to zero
+    return showList();
 
-  function randomImage2(){
-    var randomIndex2 = Math.floor(Math.random() * ProductImage.all.length);
-    ProductImage.imgEl2.src = ProductImage.all[randomIndex2].source;
-    ProductImage.imgEl2.alt = ProductImage.all[randomIndex2].name;
-    ProductImage.all[randomIndex2].timesShown += 1;
-    ProductImage.all[randomIndex2].timesEachImageClicked += 1;
-    console.log(ProductImage.all[randomIndex2].name + ' has been shown ' + ProductImage.all[randomIndex2].timesShown + ' times' + ' and clicked: ' + ProductImage.all[randomIndex2].timesEachImageClicked);
-    //random images for two-------------------------------
   }
-//  timesCycled++;
+  console.log(e.target.alt);
+  displayImages();
+  ///--------where to push clicks and images?
+}
 
-//end of if else statement for 25
-
-
-document.getElementById('the_image').addEventListener('click', randomImage);
-document.getElementById('the_image1').addEventListener('click', randomImage);
-document.getElementById('the_image2').addEventListener('click', randomImage);
-
-document.getElementById('the_image').addEventListener('click', randomImage1);
-document.getElementById('the_image1').addEventListener('click', randomImage1);
-document.getElementById('the_image2').addEventListener('click', randomImage1);
+displayImages();
 
 
-//
-document.getElementById('the_image').addEventListener('click', randomImage2);
-document.getElementById('the_image1').addEventListener('click', randomImage2);
-document.getElementById('the_image2').addEventListener('click', randomImage2);
-//
-randomImage();
-//
-randomImage1();
 
-randomImage2();
-
+ProductImage.container.addEventListener('click', handleClick);
 
 //what I would like to use to deal with the clicks/
+
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ProductImage.allNames,//push names of images
+    datasets: [{
+      label: '# of Clicks',
+      data: [3,4,5,6],//push times clicked
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
+        }
+      }]
+    }
+  }
+});
