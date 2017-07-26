238644c3f27d963fa5185385e11bf7c0f7f77c33
mstclair <stclairm@spu.edu>
2017-07-26 14:53:21 -0700

    got local storage to store click amt array

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 'use strict';
+//the array I want saved is product.clickArray
+
+
+
 
 function ProductImage(number) {
   this.name = number;
@@ -14,14 +18,16 @@ ProductImage.all = []; //array for my images
 
 ProductImage.allNames = ['bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'tauntaun', 'unicorn', 'water-can', 'wine-glass'];//pushed into array
 
-ProductImage.clickArray = [];//array for chart
+var clickArray = [];//array for chart
 ProductImage.namesChartArray = []; //array for ch
 
 
 var previouslyShown = [];
 
 for(var i = 0; i < ProductImage.allNames.length; i++){
-  new ProductImage(ProductImage.allNames[i]); //creates new object with images from all names array
+  new ProductImage(ProductImage.allNames[i]); //creates new object with images from all names array and will be moved into an else of
+  //an if statement that relates to your local storage
+
 }
 ProductImage.leftImage = document.getElementById('left');
 ProductImage.centerImage = document.getElementById('center');
@@ -30,13 +36,12 @@ ProductImage.container = document.getElementById('imageContainer');
 
 
 function makeRandomNumber(){
-  return Math.floor(Math.random() * ProductImage.all.length)
-
+  return Math.floor(Math.random() * ProductImage.all.length);
 }
 
 function displayImages() {
 
-console.log(previouslyShown, 'previouslyShown images');
+  console.log(previouslyShown, 'previouslyShown images');
 
   var numbers = [];
   numbers[0] = makeRandomNumber();
@@ -78,26 +83,32 @@ function showList(){
 
 }
 
+//pushNamesAndClicks();
 function handleClick(e){
+
+  if(e.target.id === 'imageContainer'){
+    return alert('you missed! click the image please!'); //return will kick us out of function
+  }
+
   ProductImage.totalClicks += 1;
-  //ProductImage.clickArray.push();
+  //clickArray.push();
   console.log(ProductImage.totalClicks);
   for(var i = 0; i < ProductImage.all.length; i ++)
   {
     if(e.target.alt === ProductImage.all[i].name){
       ProductImage.all[i].timesClicked += 1;
-      ProductImage.clickArray[i] = ProductImage.all[i].timesClicked;
+      clickArray[i] = ProductImage.all[i].timesClicked;
     }
   }
   if(ProductImage.totalClicks === 25){
     ProductImage.container.removeEventListener('click', handleClick);
-    //set source attributes to zero
+    //set source attributes to zero //this is where I will put the local storage send
+    localStorage.setItem('clickArray', JSON.stringify(clickArray));
+    localStorage.clickArray = JSON.stringify(clickArray);//local storage
     return showList();
   }
   console.log(e.target.alt);
   displayImages();
-  //pushNamesAndClicks();
-  ///--------where to push clicks and images?
 }
 
 displayImages();
@@ -109,43 +120,44 @@ ProductImage.container.addEventListener('click', handleClick);
 //what I would like to use to deal with the clicks/
 
 function createChart () {
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ProductImage.allNames,//push names of images
-    datasets: [{
-      label: '# of Clicks',
-      data:  ProductImage.clickArray,//ProductImage.clickArray,//push times clicked
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero:true
-        }
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ProductImage.allNames,//push names of images
+      datasets: [{
+        label: '# of Clicks',
+        data:  clickArray,//clickArray,//push times clicked
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
       }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
     }
-  }
-});
+  });
 }
 
 document.getElementById('chartButton').addEventListener('click', createChart);
+console.log(JSON.parse(localStorage.getItem(clickArray)));
